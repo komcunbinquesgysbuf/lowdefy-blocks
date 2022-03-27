f34ba0d50109cd92b3095c6ad0e6bcf9297420f2
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-03-27 14:22:35 +0000

    Remove example code

--- src/index.js
+++ src/index.js
@@ -14,12 +14,7 @@
   limitations under the License.
 */
 
-import ContainerBlock from './blocks/ContainerBlock/ContainerBlock';
-import ContextBlock from './blocks/ContextBlock/ContextBlock';
-import DisplayBlock from './blocks/DisplayBlock/DisplayBlock';
-import InputBlock from './blocks/InputBlock/InputBlock';
 import FileReader from './blocks/FileReader/FileReader';
-import ListBlock from './blocks/ListBlock/ListBlock';
 
-export { ContainerBlock, ContextBlock, DisplayBlock, InputBlock, FileReader, ListBlock };
-export default { ContainerBlock, ContextBlock, DisplayBlock, InputBlock, FileReader, ListBlock };
+export {FileReader};
+export default {FileReader};
