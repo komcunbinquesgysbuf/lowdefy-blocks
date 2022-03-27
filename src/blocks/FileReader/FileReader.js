ca5fcf27c9740b42af3f99ac6266cb394e6fcbff
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-03-27 12:27:29 +0000

    Use structure and class names from other ant controls

--- src/blocks/FileReader/FileReader.js
+++ src/blocks/FileReader/FileReader.js
@@ -2,24 +2,33 @@ import React from 'react';
 import {blockDefaultProps} from '@lowdefy/block-tools';
 
 const FileUpload = ({blockId, properties, methods, value}) => (
-    <label>
-        {properties.label || 'Label'}:
-        <input
-            data-testid={`${blockId}-input`}
-            id={blockId}
-            name='file'
-            type="file"
-            onChange={({target: {files: [file]}}) =>
-                (reader => {
-                    reader.onload = ({target: {result}}) => methods.setValue({
-                        name: file.name,
-                        data: result.replace(/^data:.*;base64,/, '')
-                    });
-                    reader.readAsDataURL(file);
-                })(new FileReader())
-            }
-        />
-    </label>
+    <div id={blockId} className='ant-row ant-form-item' style={{marginBottom: 0}}>
+        <div className='ant-col ant-form-item-label ant-form-item-label-left css-vqci06 ant-col-xs-24 ant-col-sm-24'>
+            <label htmlFor={`${blockId}-input`} title={properties.label}>
+                <span>{properties.label}</span>
+            </label>
+        </div>
+        <div className='ant-col ant-form-item-control ant-col-xs-24 ant-col-sm-24'>
+            <div className="ant-form-item-control-input">
+                <div className="ant-form-item-control-input-content">
+                    <input
+                        id={`${blockId}-input`}
+                        name='file'
+                        type="file"
+                        onChange={({target: {files: [file]}}) =>
+                            (reader => {
+                                reader.onload = ({target: {result}}) => methods.setValue({
+                                    name: file.name,
+                                    data: result.replace(/^data:.*;base64,/, '')
+                                });
+                                reader.readAsDataURL(file);
+                            })(new FileReader())
+                        }
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
 );
 
 FileUpload.defaultProps = blockDefaultProps;
